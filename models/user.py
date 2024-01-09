c79f7c95fa8c1a6af3ba4767586fb6c7cf2b2d3c
mo7amedElfadil <mohamedelfadil1994@gmail.com>
2024-01-09 18:04:38 +0200

    created new classes and updated file storage to dynamically store them when saving

---
+++ models/user.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""user module. Contains class User
+Inherits from BaseModel class
+"""
+from models.base_model import BaseModel
+
+class User(BaseModel):
+    """User class
+
+    Attributes:
+        email (public, class attribute): string, default empty -> ""
+        password (public, class attribute): string, default empty -> ""
+        first_name (public, class attribute): string, default empty -> ""
+        last_name (public, class attribute): string, default empty -> ""
+    """
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
