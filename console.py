0f5acd2163baddd6ceefdd8ab0ede50ff6e1fa34
sixthson6 <sudais2408@gmail.com>
2024-01-09 16:03:40 +0000

    added do_create and do_show

--- console.py
+++ console.py
@@ -2,7 +2,9 @@
 """
 Entry point into the AirBnB console app
 """
-import cmd
+import cmd, shlex, models
+from models.base_model import BaseModel
+# from models.engine.file_storage import FileStorage
 
 
 class HBNBCommand(cmd.Cmd):
@@ -11,12 +13,48 @@ class HBNBCommand(cmd.Cmd):
     quit, EOF, help
     """
     prompt = "(hbnb) "
+    class_name = {'BaseModel': BaseModel}
 
     # pylint: disable-next=unused-argument
     def do_quit(self, arg) -> bool:
         """Quit command to exit the program
         """
         return True
+
+    def do_create(self, arg):
+        """
+        create command to create new instance of BaseModel
+        and save it as a json file
+        """
+        if not arg:
+            print("** class name is missing **")
+        elif arg not in self.class_name.keys():
+            print("** class doesn't exit **")
+        else:
+            new_instance = self.class_name[arg]()
+            # new_instance.to_json()
+            print(new_instance.id)
+
+    def do_show(self, arg):
+        """
+        Show command to print the str representation of an instance
+        based on class name and id
+        """
+        arguments = shlex.split(arg)
+        if len(arguments) < 2:
+            print("** instance id is missing **")
+        if not arg:
+            print("** class is missing **")
+        elif arguments[0] != "BaseModel":
+            print("** class doesn't exit **")
+        elif arguments[0] + '.' + arguments[1] not in\
+                models.storage._FileStorage__objects.keys():
+                    print("** no instance found **")
+        else:
+            new_instance = self.class_name[arguments[0]]()
+            print(new_instance.__str__())
+
+
 
     do_EOF = do_quit
 
